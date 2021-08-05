34291d66fe310b310d71b2b9bb1286a242b964f3
habatoo <habatoo@gmail.com>
2021-08-05 21:15:42 +0700

    working version

---
+++ Main.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("hello Gradle"));
+		System.out.println(StringUtils.upperCase("Hello Gradle"));
+	}
+}
---
+++ Makefile
@@ -0,0 +1,3 @@
+start:
+	javac -cp ".:commons-lang3-3.12.0/commons-lang3-3.12.0.jar" -d . Main.java
+	java -cp ".:commons-lang3-3.12.0/commons-lang3-3.12.0.jar" Main
