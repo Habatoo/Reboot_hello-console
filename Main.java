20c5db8277b32b9093c80d418242bc22e7401b97
habatoo <habatoo@gmail.com>
2021-08-05 21:34:54 +0700

    fix Main

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import org.apache.commons.lang3.StringUtils;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(StringUtils.capitalize("hello Gradle"));
-		System.out.println(StringUtils.upperCase("Hello Gradle"));
+		System.out.println(StringUtils.capitalize("hello console"));
+		System.out.println(StringUtils.upperCase("Hello Console"));
 	}
 }
