4494b9d0c72a9939f7d19816284dc1513d1bd6ad
habatoo <habatoo@gmail.com>
2021-08-06 17:39:58 +0700

    new feature

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(StringUtils.capitalize("hello console"));
+		System.out.println(StringUtils.capitalize("hello new feature"));
 		System.out.println(StringUtils.upperCase("Hello Console"));
 	}
 }
